0b4548e283f1374aab525d29ffb375a50021f35b
imPROMPTu <akshayp8@illinois.edu>
2026-05-26 17:04:37 +0000

    chore: scaffold Vite + React + TS + Tailwind + Vitest

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        app: '#0f0f13',
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { backgroundPosition: '-1000px 0' },
+          '100%': { backgroundPosition: '1000px 0' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 2s infinite linear',
+      },
+    },
+  },
+  plugins: [],
+}
